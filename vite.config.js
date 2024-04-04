23bd4cafe6464c3818831fe32ad6b45273622ef6
Jamzcode <129902520+Jamzcode@users.noreply.github.com>
2024-04-04 17:58:42 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bug-wars-Frontend/',
+  // base: '/bug-wars-Frontend/',
   plugins: [vue()],
   resolve: {
     alias: {
